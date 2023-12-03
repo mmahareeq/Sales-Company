bba2cb3691c05e6a277192410166366cf7ace8cb
mmahareeq <mmjaryq@gmail.com>
2023-12-03 22:04:21 +0200

    implement login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  loginForm !: FormGroup;
+  ngOnInit(): void {
+    
+    this.loginForm = new FormGroup({
+      email: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    })
+  }
+  
+  onSubmit(){
+    
+  }
 }
